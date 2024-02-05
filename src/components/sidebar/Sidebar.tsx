5a1eb482b8070148f2385420dbe4d4c5dc229710
krishna <krishnakushwaha743@gmail.com>
2024-02-05 10:41:07 +0530

    remove commented data

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,16 +1,14 @@
-import { Menu } from 'antd';
-import { MdDashboard } from 'react-icons/md';
-import React from 'react';
-import { NavLink, useLocation, useNavigate } from 'react-router-dom';
-// import Logo from '../assets/icons/LOGO .png';
-import Cookies from 'js-cookie';
+import { Menu } from "antd";
+import { MdDashboard } from "react-icons/md";
+import { IoIosLogOut } from "react-icons/io";
+
+import React from "react";
+import { NavLink, useLocation, useNavigate } from "react-router-dom";
+import Logo from "../../assets/icons/krishna_k.png";
+import Cookies from "js-cookie";
 // import LOGIN_ICON from '../assets/icons/login.png';
-import { BsBookHalf } from 'react-icons/bs';
-import { MdOutlineInventory2 } from 'react-icons/md';
-import { SiGooglemarketingplatform } from 'react-icons/si';
-import { FaRankingStar } from 'react-icons/fa6';
-import { VscPreview } from 'react-icons/vsc';
-import { IoSettingsOutline } from 'react-icons/io5';
+import { BsBookHalf } from "react-icons/bs";
+
 
 const Sidebar = () => {
   const navigate = useNavigate();
@@ -18,17 +16,16 @@ const Sidebar = () => {
 
   const menuItems = [
     {
-      path: '/dashboard',
-      icon: <MdDashboard style={{ fontSize: '20px' }} />,
-      label: 'Dashboard',
+      path: "/dashboard",
+      icon: <MdDashboard style={{ fontSize: "20px" }} />,
+      label: "Dashboard",
     },
     {
-      path: '/settings',
-      icon: <BsBookHalf style={{ fontSize: '20px' }} />,
-      label: 'Settings',
+      path: "/settings",
+      icon: <BsBookHalf style={{ fontSize: "20px" }} />,
+      label: "Settings",
     },
-    
-   
+
     // Add other menu items here if needed
   ];
 
@@ -37,23 +34,22 @@ const Sidebar = () => {
   };
 
   const handleLogout = () => {
-    Cookies.remove('token');
-    navigate('/');
+    Cookies.remove("token");
+    navigate("/");
   };
 
   return (
     <div
       className="flex flex-col justify-between gap-4 text-white p-4 py-8"
-      style={{ backgroundColor: 'black', height: '100vh' }}
+      style={{ backgroundColor: "black", height: "100vh" }}
     >
       <div>
-        <div className="text-center mb-8">
-          {/* Replace 'company-logo.png' with your actual company logo */}
-          {/* <img src={Logo} alt="Logo" /> */}
+        <div className="text-center">
+          <img src={Logo} alt="Logo" className="h-full" />
         </div>
 
         <Menu
-          style={{ backgroundColor: 'black' }}
+          style={{ backgroundColor: "black" }}
           mode="inline"
           defaultSelectedKeys={[location.pathname]}
           selectedKeys={[location.pathname]}
@@ -64,23 +60,23 @@ const Sidebar = () => {
               key={path}
               icon={React.cloneElement(icon, {
                 style: {
-                  fontSize: '20px',
-                  color: location.pathname === path ? '#FF5612' : 'white',
+                  fontSize: "20px",
+                  color: location.pathname === path ? "#000" : "white",
                 },
               })}
               className={`mb-2 ${
-                location.pathname === path ? 'ant-menu-item-selected' : ''
+                location.pathname === path ? "ant-menu-item-selected" : ""
               }`}
               style={{
                 backgroundColor:
-                  location.pathname === path ? '#FFE4E1' : 'transparent',
+                  location.pathname === path ? "#ffff" : "transparent",
               }}
             >
               <NavLink
                 to={path}
                 className="font-semibold text-lg"
                 style={{
-                  color: location.pathname === path ? '#FF5612' : 'white',
+                  color: location.pathname === path ? "black" : "white",
                 }}
               >
                 {label}
@@ -89,25 +85,14 @@ const Sidebar = () => {
           ))}
         </Menu>
       </div>
-
       {/* Logout Button */}
-      <Menu style={{ backgroundColor: '#FF5612', borderRadius: '10px' }}>
+      <Menu style={{ backgroundColor: "#fff", borderRadius: "10px" }}>
         <Menu.Item
-          icon={
-            <img
-              // src={LOGIN_ICON}
-              alt="Logout"
-              style={{
-                fontSize: '20px',
-                color: 'white',
-              }}
-              width={23}
-            />
-          }
+          icon={<IoIosLogOut size={20} />}
           className="text-center"
           onClick={handleLogout}
         >
-          <span className="font-semibold text-lg text-white rounded">
+          <span className="font-semibold text-lg text-black rounded ">
             Logout
           </span>
         </Menu.Item>
